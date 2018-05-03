37bd8e2fcabef8482fce775fef491588ef55a607
Kata1213 <2726456039@qq.com>
2018-05-03 21:16:53 +0800

    demoio

---
+++ src/test/java/com/TW/Homewoek/Kata1213/SpringBoot/example/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.TW.Homewoek.Kata1213.SpringBoot.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
